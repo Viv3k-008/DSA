e8d8b99507271a19d51e2a666d2ffb3a59267a07
Viv3k-008 <146721350+Viv3k-008@users.noreply.github.com>
2025-11-28 18:11:06 +0530

    Time: 610 ms (5.03%) | Memory: 301.6 MB (6.38%) - LeetSync

--- 3653-maximum-subarray-sum-with-length-divisible-by-k/maximum-subarray-sum-with-length-divisible-by-k.cpp
+++ 3653-maximum-subarray-sum-with-length-divisible-by-k/maximum-subarray-sum-with-length-divisible-by-k.cpp
@@ -2,30 +2,16 @@ class Solution {
 public:
     long long maxSubarraySum(vector<int>& nums, int k) {
         map<int,set<long long>> mp;
-
+        mp[0].insert(0);
         long long sum = 0 , ans = LLONG_MIN;
         for(int i = 0 ; i < nums.size() ; i++){
             sum += nums[i];
 
-            int rem = (i)%k;
+            int rem = (i+1)%k;
             if(mp.find(rem) != mp.end()){
                 ans = max(ans , sum - *mp[rem].begin());
             }
             mp[rem].insert(sum);
-        }
-        mp.clear();
-        sum = 0;
-        for(int i = nums.size()-1 ; i >= 0 ; i--){
-            sum += nums[i];
-
-            int rem = (i)%k;
-            if(mp.find(rem) != mp.end()){
-                ans = max(ans , sum - *mp[rem].begin());
-            }
-            mp[rem].insert(sum);
-        }
-        if(nums.size()%k == 0){
-            ans = max(ans , sum);
         }
         return ans;
     }
