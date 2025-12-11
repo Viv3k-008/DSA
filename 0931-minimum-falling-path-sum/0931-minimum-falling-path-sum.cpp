2efaaade0fd9ca20eef17749540575804e3ff460
Viv3k-008 <146721350+Viv3k-008@users.noreply.github.com>
2025-12-11 12:00:53 +0530

    Time: 5 ms (12.42%), Space: 16.3 MB (6.53%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -1,24 +1,28 @@
 class Solution {
 public:
-    int minFallingPathSum(vector<vector<int>>& matrix) {
-        int n = matrix.size() , m = matrix[0].size() , ans = INT_MAX;
-        vector<vector<int>> dp;
-        dp.resize(n+1 , vector<int>(m , 0));
-        
-        for(int i = n-1 ; i >= 0 ; i--){
-            for(int j = m-1 ; j >= 0 ; j--){
-
-                int down = 0 , diagLeft = 1e9 , diagRight = 1e9;
-                down = dp[i+1][j];
-                if(j-1 >= 0) diagLeft = dp[i+1][j-1];
-                if(j+1 < m) diagRight = dp[i+1][j+1];
-
-                dp[i][j] = min({down , diagLeft , diagRight}) + matrix[i][j];
-            }
+    vector<vector<int>> dp;
+    int fn(int i , int j , vector<vector<int>>& mat){
+        if(i == mat.size()){
+            return 0;
         }
 
+        if(i < 0 || j < 0 || j >= mat[0].size()){
+            return INT_MAX;
+        }
+
+        if(dp[i][j] != -1e9) return dp[i][j];
+
+        int down = fn(i+1 , j , mat);
+        int diagLeft = fn(i+1 , j-1 , mat);
+        int diagRight = fn(i+1 , j+1 , mat);
+
+        return dp[i][j] = min({down , diagLeft , diagRight}) + mat[i][j];
+    }
+    int minFallingPathSum(vector<vector<int>>& matrix) {
+        int n = matrix[0].size() , ans = INT_MAX;
+        dp.resize(n , vector<int>(n , -1e9));
         for(int i = 0 ; i < n ; i++){
-            ans = min(ans , dp[0][i]);
+            ans = min(ans , fn(0 , i , matrix));
         } 
 
         return ans;
