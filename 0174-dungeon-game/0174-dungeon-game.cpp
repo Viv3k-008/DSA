27646f4c958b42ef488058b02864f5881c3b8ba7
Viv3k-008 <146721350+Viv3k-008@users.noreply.github.com>
2025-12-17 11:22:12 +0530

    Time: 0 ms (100%), Space: 13.2 MB (20.06%) - LeetHub

--- 0174-dungeon-game/0174-dungeon-game.cpp
+++ 0174-dungeon-game/0174-dungeon-game.cpp
@@ -23,10 +23,32 @@ public:
         }
         return dp[i][j] = req - grid[i][j];
     }
-    int calculateMinimumHP(vector<vector<int>>& dungeon) {
-        n = dungeon.size() , m = dungeon[0].size();
-        vector<vector<int>> dp(n , vector<int>(m , -1));
+    int calculateMinimumHP(vector<vector<int>>& grid) {
+        n = grid.size() , m = grid[0].size();
+        vector<vector<int>> dp(n+1 , vector<int>(m+1 , 1e9));
 
-        return fn(0 , 0 , dungeon , dp);
+        for(int i = n-1 ; i >= 0 ; i--){
+            for(int j = m-1 ; j >= 0 ; j--){
+                if(i == n-1 && j == m-1){
+                    if(grid[i][j] <= 0){
+                        dp[i][j] = abs(grid[i][j])+1;
+                    }
+                    else dp[i][j] = 1;
+                    continue;
+                }
+
+                int down = dp[i+1][j];
+                int right = dp[i][j+1];
+
+                int req = min(down,right);
+
+                if(grid[i][j] >= req){
+                    dp[i][j] = 1;
+                }
+                else dp[i][j] = req-grid[i][j];
+            }
+        }
+
+        return dp[0][0];
     }
 };
