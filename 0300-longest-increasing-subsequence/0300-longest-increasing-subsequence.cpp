4e8e0617ece3000d89c7131ac671fb9b5b0a5eeb
Viv3k-008 <146721350+Viv3k-008@users.noreply.github.com>
2026-01-08 10:54:02 +0530

    Time: 243 ms (33.89%), Space: 296.5 MB (18.86%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,22 +1,32 @@
 class Solution {
 public:
-    int fn(int i , vector<int>& nums , int prev , vector<vector<int>>& dp){
-        if(i == nums.size()){
-            return 0;
-        }
+    // int fn(int i , vector<int>& nums , int prev , vector<vector<int>>& dp){
+    //     if(i == nums.size()){
+    //         return 0;
+    //     }
 
-        if(dp[i][prev+1] != -1) return dp[i][prev+1];
+    //     if(dp[i][prev+1] != -1) return dp[i][prev+1];
 
-        int op1 = 0;
-        if(prev == -1 || nums[i] > nums[prev]) op1 = fn(i+1 , nums , i , dp) + 1;
-        int op2 = fn(i+1 , nums , prev , dp);
+    //     int op1 = 0;
+    //     if(prev == -1 || nums[i] > nums[prev]) op1 = fn(i+1 , nums , i , dp) + 1;
+    //     int op2 = fn(i+1 , nums , prev , dp);
 
-        return dp[i][prev+1] = max(op1 , op2);
-    }
+    //     return dp[i][prev+1] = max(op1 , op2);
+    // }
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
 
-        vector<vector<int>> dp(n , vector<int>(n+1 , -1));
-        return fn(0 , nums , -1 , dp);
+        vector<vector<int>> dp(n+1 , vector<int>(n+1 , 0));
+        
+        for(int i = n-1 ; i >= 0 ; i--){
+            for(int prev = i-1 ; prev >= -1 ; prev--){
+                if(prev == -1 || nums[i] > nums[prev]){
+                    dp[i][prev+1] = max(1+dp[i+1][i+1] , dp[i+1][prev+1]);
+                }
+                else dp[i][prev+1] = dp[i+1][prev+1];
+            }
+        }
+
+        return dp[0][0];
     }
 };
