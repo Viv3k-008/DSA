90fb7fca80f76bbfe2386468bcaef1d3b616708e
Viv3k-008 <146721350+Viv3k-008@users.noreply.github.com>
2026-02-05 10:20:36 +0530

    Time: 11 ms (18.11%), Space: 25.8 MB (39.55%) - LeetHub

--- 3379-transformed-array/3379-transformed-array.cpp
+++ 3379-transformed-array/3379-transformed-array.cpp
@@ -6,9 +6,9 @@ public:
         vector<int> ans(n , 0);
 
         for(int i = 0; i < n; i++){
-            long long idx = ( (long long)i + nums[i] ) % n;
-            if(idx < 0) idx += n;
-            ans[i] = nums[idx];
+            int newi = (((i + nums[i]) % n) + n)%n;
+
+            ans[i] = nums[newi];
         }
 
         return ans;
