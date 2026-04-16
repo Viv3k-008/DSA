d1c284431332a78931f90bbbf3f76d90606a3eca
Viv3k-008 <146721350+Viv3k-008@users.noreply.github.com>
2026-04-16 19:22:39 +0530

    Time: 0 ms (100%), Space: 10.5 MB (60.06%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,27 +1,23 @@
 class Solution {
 public:
-    // int fn(int i , vector<int>& nums , vector<int>& dp){
-    //     if(i >= nums.size()){
-    //         return 0;
-    //     }
-    //     if(dp[i] != -1) return dp[i];
+    vector<int> dp;
+    int fn(int i , vector<int>& nums){
+        if(i >= nums.size()){
+            return 0;
+        }
 
-    //     int op1 = fn(i+2 , nums , dp) + nums[i];
-    //     int op2 = fn(i+1 , nums , dp);
+        if(dp[i] != -1) return dp[i];
 
-    //     return dp[i] = max(op1 , op2);
-    // }
+        int op1 = nums[i] + fn(i+2 , nums);
+        int op2 = fn(i+1 , nums);
+
+        return dp[i] = max(op1 , op2);
+    }
     int rob(vector<int>& nums) {
         int n = nums.size();
 
-        int next = 0 , secNext = 0;
-        for(int i = n-1 ; i >= 0 ; i--){
-            int cur = max(secNext+nums[i] , next);
-
-            secNext = next;
-            next = cur;
-        }
-
-        return next;
+        dp.assign(n , -1);
+        
+        return fn(0 , nums);
     }
 };
