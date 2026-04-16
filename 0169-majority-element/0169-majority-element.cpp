6da8b0ef824de4c1b3e0c26b5be25280cfea18f7
Viv3k-008 <146721350+Viv3k-008@users.noreply.github.com>
2026-04-16 18:37:50 +0530

    Time: 0 ms (100%), Space: 28.1 MB (87.06%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -3,15 +3,15 @@ public:
     int majorityElement(vector<int>& nums) {
         int n = nums.size();
 
-        int cnt = 0 , ele = -1;
+        int ele = -1 , cnt = 0;
 
         for(int i = 0 ; i < n ; i++){
-            if(cnt == 0){
-                cnt = 1;
-                ele = nums[i];
-            }
-            else if(nums[i] == ele){
+            if(ele == nums[i]){
                 cnt++;
+            }
+            else if(cnt == 0){
+                ele = nums[i];
+                cnt = 1;
             }
             else cnt--;
         }
