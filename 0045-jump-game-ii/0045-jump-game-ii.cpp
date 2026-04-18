036c4dd44e60be60b9b903b161d660db36e89325
Viv3k-008 <146721350+Viv3k-008@users.noreply.github.com>
2026-04-18 23:11:30 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,27 +1,21 @@
 class Solution {
 public:
-    int fn(int i , vector<int>& nums , vector<int>& dp){
-        if(i == nums.size()-1){
-            return 0;
-        }
-
-        if(dp[i] != -1) return dp[i];
-
-        int cur = 1e9;
-        for(int k = 1 ; k <= nums[i] ; k++){
-
-            if(i+k >= nums.size()) continue;
-
-            int temp = 1 + fn(i+k , nums , dp);
-
-            cur = min(temp , cur);
-        }
-
-        return dp[i] = cur;
-    }
     int jump(vector<int>& nums) {
         int n = nums.size();
-        vector<int> dp(n , -1);
-        return fn(0 , nums , dp);
+
+        int maxRange = 0 , curEnd = 0 , jumps = 0;
+
+        for(int i = 0 ; i < n ; i++){
+            maxRange = max(maxRange , i+nums[i]);
+
+            if(i == curEnd){
+                jumps++;
+                curEnd = maxRange;
+            }
+
+            if(maxRange >= n-1) break;
+        }
+
+        return jumps+1;
     }
 };
