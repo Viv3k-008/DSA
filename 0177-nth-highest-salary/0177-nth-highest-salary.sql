92e24cbc4377e53d325c64df53261b12fa7c12ae
Viv3k-008 <146721350+Viv3k-008@users.noreply.github.com>
2026-04-18 10:11:04 +0530

    Time: 843 ms (6.6%), Space: 0B (100%) - LeetHub

--- 0177-nth-highest-salary/0177-nth-highest-salary.sql
+++ 0177-nth-highest-salary/0177-nth-highest-salary.sql
@@ -1,12 +1,13 @@
 CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
 BEGIN
   RETURN (
-        SELECT distinct Salary
-        FROM Employee E1
-        WHERE (
-            SELECT COUNT(DISTINCT E2.Salary)
-            FROM Employee E2
-            WHERE E2.Salary > E1.Salary
-        ) = N-1
+     select distinct e1.salary
+     from Employee e1
+     where (
+        select count(distinct e2.salary)
+        from Employee e2
+        where e2.salary > e1.salary
+     ) = N-1
+
   );
 END
