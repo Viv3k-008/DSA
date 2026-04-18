bfbc77a7d468e4d7897ec5928a2bf187fc7108c1
Viv3k-008 <146721350+Viv3k-008@users.noreply.github.com>
2026-04-18 22:44:19 +0530

    Time: 2 ms (27.92%), Space: 52.4 MB (56.41%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n = nums.size();
+
+        int pt = n-1;
+
+        for(int i = n-2 ; i >= 0 ; i--){
+            if(i+nums[i] >= pt){
+                pt = i;
+            }
+        }
+
+        return pt == 0;
+    }
+};
