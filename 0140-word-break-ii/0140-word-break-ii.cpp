1eeba239caab008bbec57368a3b026ff1e446887
Viv3k-008 <146721350+Viv3k-008@users.noreply.github.com>
2026-04-20 22:48:30 +0530

    Time: 0 ms (100%), Space: 11.1 MB (20.36%) - LeetHub

--- 0140-word-break-ii/0140-word-break-ii.cpp
+++ 0140-word-break-ii/0140-word-break-ii.cpp
@@ -3,32 +3,32 @@ public:
     unordered_map<int, vector<string>> dp;
 
     vector<string> solve(int i, string &s, unordered_set<string> &st) {
-        // if already computed
-        if(dp.count(i)) return dp[i];
+       
+       if(dp.count(i)){
+        return dp[i];
+       }
 
-        vector<string> res;
+       vector<string> res;
+       if(i == s.size()){
+          res.push_back("");
+          return res;
+       }
 
-        // reached end
-        if(i == s.size()) {
-            res.push_back("");
-            return res;
-        }
+       string temp = "";
+       for(int k = i ; k < s.size() ; k++){
+          temp += s[k];
 
-        string temp = "";
+          if(st.count(temp)){
+            vector<string> next = solve(k+1 , s , st);
 
-        for(int j = i; j < s.size(); j++) {
-            temp += s[j];
-
-            // if word found
-            if(st.count(temp)) {
-                vector<string> next = solve(j + 1, s, st);
-
-                for(string x : next) {
-                    if(x == "") res.push_back(temp);
-                    else res.push_back(temp + " " + x);
+            for(string x : next){
+                if(x == ""){
+                    res.push_back(temp);
                 }
+                else res.push_back(temp + " " + x);
             }
-        }
+          }
+       }
 
         return dp[i] = res;
     }
