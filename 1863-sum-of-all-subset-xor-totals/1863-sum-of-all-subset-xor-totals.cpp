4c5dc8acb4f4150cab19cd5d669fefdf63c512b9
Viv3k-008 <146721350+Viv3k-008@users.noreply.github.com>
2026-04-20 12:27:54 +0530

    Time: 0 ms (100%), Space: 9.7 MB (16.44%) - LeetHub

--- 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.cpp
+++ 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
+    vector<vector<int>> dp;
     int fn(int i , int xr , vector<int>& nums){
         if(i == nums.size()){
             return xr;
         }
 
-
+        if(dp[i][xr] != -1) return dp[i][xr];
         int op1 = fn(i+1 , xr^nums[i] , nums);
         int op2 = fn(i+1 , xr , nums);
 
-        return op1+op2;
+        return dp[i][xr] = op1+op2;
     }
     int subsetXORSum(vector<int>& nums) {
-        
+        int n = nums.size();
+        dp.assign(n , vector<int>(32 , -1));
         return fn(0 , 0 , nums);
     }
 };
