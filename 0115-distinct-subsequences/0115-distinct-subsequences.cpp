e6d5e64a58c8c357932b65e6fe2fe99c5e87c3d6
Viv3k-008 <146721350+Viv3k-008@users.noreply.github.com>
2026-04-20 17:07:41 +0530

    Time: 42 ms (15.64%), Space: 26.9 MB (62.87%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,29 +1,23 @@
 class Solution {
 public:
-    vector<vector<int>> dp;
-    int count(int i , int j , string& s , string& t){
-        int n = s.size();
-        int m = t.size();
-        if(j == m){
-            return 1;
-        }
-        else if(i == n) return 0;
+    int fn(int i , int j , int n , int m , string& s , string& t , vector<vector<int>>& dp){
+        if(j == m) return 1;
+        if(i >= n) return 0;
 
         if(dp[i][j] != -1) return dp[i][j];
 
-        int op1 = 0 , op2 = 0;
+        int op1 = 0;
         if(s[i] == t[j]){
-            op1 = count(i+1 , j , s , t) + count(i+1 , j+1 , s , t);
+            op1 = fn(i+1 , j+1 , n , m , s , t , dp);
         }
-        else {
-            op2 = count(i+1 , j , s , t);
-        }
-        return dp[i][j] = op1+op2;
+        int op2 = fn(i+1 , j , n , m , s , t , dp);
+
+        return dp[i][j] = op1 + op2;
     }
     int numDistinct(string s, string t) {
-        int n = s.size();
-        int m = t.size();
-        dp.resize(n+1 , vector<int>(m+1 , -1));
-        return count(0 , 0 , s , t);
+        int n = s.size() , m = t.size();
+
+        vector<vector<int>> dp(n , vector<int>(m , -1));
+        return fn(0 , 0 , n , m , s , t , dp);
     }
 };
