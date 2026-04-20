601d875aca3e1acd1726bbc7f0cd86ae7ccbdb15
Viv3k-008 <146721350+Viv3k-008@users.noreply.github.com>
2026-04-20 16:25:11 +0530

    Time: 0 ms (100%), Space: 12.9 MB (72.92%) - LeetHub

--- 0174-dungeon-game/0174-dungeon-game.cpp
+++ 0174-dungeon-game/0174-dungeon-game.cpp
@@ -1,56 +1,27 @@
 class Solution {
 public:
-    int n , m;
-    int fn(int i , int j , vector<vector<int>>& grid , vector<vector<int>>& dp){
-        if(i >= n || j >= m){
-            return 1e9;
-        }
+    int fn(int i , int j , vector<vector<int>>& grid , int n , int m , vector<vector<int>>& dp){
         if(i == n-1 && j == m-1){
-            if(grid[i][j] <= 0){
-                return abs(grid[i][j])+1;
-            }
-            else return 1;
+            if(grid[i][j] > 0) return 1;
+            else return abs(grid[i][j])+1;
         }
+        else if(i >= n || j >= m) return 1e9;
 
-        if(dp[i][j] != -1) return dp[i][j];
+        if(dp[i][j] != 1e9) return dp[i][j];
 
-        int down = fn(i+1 , j , grid , dp);
-        int right = fn(i , j+1 , grid , dp);
+        int op1 = fn(i+1 , j , grid , n , m , dp);
+        int op2 = fn(i , j+1 , grid , n , m , dp);
 
-        int req = min(down , right);
+        int req = min(op1 , op2);
         if(grid[i][j] >= req){
             return dp[i][j] = 1;
         }
-        return dp[i][j] = req - grid[i][j];
+        else return dp[i][j] = abs(grid[i][j] - req);
     }
-    int calculateMinimumHP(vector<vector<int>>& grid) {
-        n = grid.size() , m = grid[0].size();
-        // vector<vector<int>> dp(n+1 , vector<int>(m+1 , 1e9));
-        vector<int> cur(m+1 , 1e9) , next(m+1 , 1e9);
+    int calculateMinimumHP(vector<vector<int>>& dungeon) {
+        int n = dungeon.size() , m = dungeon[0].size();
 
-        for(int i = n-1 ; i >= 0 ; i--){
-            for(int j = m-1 ; j >= 0 ; j--){
-                if(i == n-1 && j == m-1){
-                    if(grid[i][j] <= 0){
-                        cur[j] = abs(grid[i][j])+1;
-                    }
-                    else cur[j] = 1;
-                    continue;
-                }
-
-                int down = next[j];
-                int right = cur[j+1];
-
-                int req = min(down,right);
-
-                if(grid[i][j] >= req){
-                    cur[j] = 1;
-                }
-                else cur[j] = req-grid[i][j];
-            }
-            next = cur;
-        }
-
-        return next[0];
+        vector<vector<int>> dp(n , vector<int>(m , 1e9));
+        return fn(0 , 0 , dungeon , n , m , dp);
     }
 };
