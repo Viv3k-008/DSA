36bf3cd46d7ebce51d4d018c96a5e104f9a02366
Viv3k-008 <146721350+Viv3k-008@users.noreply.github.com>
2026-05-27 14:42:01 +0530

    Time: 77 ms (41.57%), Space: 25.4 MB (30.62%) - LeetHub

--- 3121-count-the-number-of-special-characters-ii/3121-count-the-number-of-special-characters-ii.cpp
+++ 3121-count-the-number-of-special-characters-ii/3121-count-the-number-of-special-characters-ii.cpp
@@ -3,7 +3,7 @@ public:
     int numberOfSpecialChars(string word) {
         int n = word.size();
 
-        unordered_map<char,int> mp , flag;
+        unordered_map<char,int> mp;
 
         for(int i = 0 ; i < n ; i++){
            if(word[i] >= 97) mp[word[i]] = i;
@@ -13,9 +13,8 @@ public:
         int cnt = 0;
         for(pair<char,int> it : mp){
             if(it.first >= 97){
-                if(mp.count(it.first-32) && !flag.count(it.first) && mp[it.first-32] > mp[it.first]){
+                if(mp.count(it.first-32) && mp[it.first-32] > mp[it.first]){
                     cnt++;
-                    flag[it.first]++;
                 }
             }
         }
