4ba223e9b2d0a515978ffecef9c67cb3592aad96
Vivek Kumar Singh <singhvivg2n@gmail.com>
2026-07-09 14:01:05 +0530

    Time: 12 ms (69.59%), Space: 224.1 MB (78.36%) - LeetHub

--- 3532-path-existence-queries-in-a-graph-i/3532-path-existence-queries-in-a-graph-i.cpp
+++ 3532-path-existence-queries-in-a-graph-i/3532-path-existence-queries-in-a-graph-i.cpp
@@ -1,51 +1,10 @@
-class DisjointSetUnion{
-public:
-     vector<int> parent;
-     vector<int> size;
-     int components;
-       
-       DisjointSetUnion(int n){
-         components = n;
-         parent.resize(n);
-         for(int i = 0 ; i < n ; i++) parent[i] = i;
-         size.resize(n,1);
-       }
-
-       int root(int i){
-          if(i == parent[i]){
-            return i;
-          }
-
-          return parent[i] = root(parent[i]);
-       }
-
-       bool find(int u, int v){
-        return root(u) == root(v);
-       }
-
-       bool UnionNode(int a, int b){
-        int u = root(a);
-        int v = root(b);
-
-        if(u == v) return false;
-
-        if(size[u] > size[v]){
-            swap(u,v);
-        }
-
-        parent[u] = v;
-        size[v] += size[u];
-        components--;
-        return true;
-       }
-    
-};
 class Solution {
 public:
     vector<bool> pathExistenceQueries(int n, vector<int>& nums, int maxDiff, vector<vector<int>>& queries) {
-        vector<vector<int>> edges;
-        DisjointSetUnion dsu(n);
-        
+
+        vector<int> id(n);
+        for(int i = 0 ; i < n ; i++) id[i] = i;
+
         int l = 0, r = 1;
         while(r < n){
             if(l == r){
@@ -53,9 +12,7 @@ public:
             }
             else{
                 if((nums[r]-nums[l]) <= maxDiff){
-                    if(!dsu.find(l,r)){
-                        dsu.UnionNode(l,r);
-                    }
+                    id[r] = id[l];
                     r++;
                 }
                 else l++;
@@ -67,7 +24,7 @@ public:
 
         for(int i = 0 ; i < m ; i++){
             int l = queries[i][0], r = queries[i][1];
-            if(dsu.find(l,r)){
+            if(id[l] == id[r]){
                 ans.push_back(true);
             }
             else ans.push_back(false);
