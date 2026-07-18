2ebf4636f172b70326d4aaf2564f4f869f342f1b
Vivek Kumar Singh <singhvivg2n@gmail.com>
2026-07-18 15:33:15 +0530

    Time: 103 ms (38.55%), Space: 129.9 MB (43.37%) - LeetHub

--- 3312-sorted-gcd-pair-queries/3312-sorted-gcd-pair-queries.cpp
+++ 3312-sorted-gcd-pair-queries/3312-sorted-gcd-pair-queries.cpp
@@ -1,26 +1,63 @@
 class Solution {
-    using ll = long long;
 public:
-    vector<int> gcdValues(vector<int>& A, vector<long long>& queries) {
-        int mx = ranges::max(A);
-        vector<int> freq(mx + 1, 0);
-        vector<ll> GCD(mx + 1, 0);
-        
-        for (auto& a : A) freq[a]++;
-        
-        for (int i = mx; i > 0; i--) {
-            ll sm = 0, extra = 0;
-            for (int j = i; j <= mx; j += i)
-                sm += freq[j], extra += GCD[j];
-            GCD[i] = sm * (sm - 1) / 2 - extra;
+    vector<int> gcdValues(vector<int>& nums, vector<long long>& queries) {
+        int n = nums.size();
+
+        int maxVal = INT_MIN;
+        for(int i : nums) maxVal = max(maxVal, i);
+
+        vector<int> divisorCnt(maxVal+1, 0);
+
+        for(int i = 0 ; i < n ; i++){
+            int num = nums[i];
+
+            for(int j = 1 ; j*j <= num ; j++){
+                if(num%j == 0){
+                    divisorCnt[j]++;
+                    if(num/j != j){
+                        divisorCnt[num/j]++;
+                    }
+                }
+            }
         }
-        
-        partial_sum(GCD.begin(), GCD.end(), GCD.begin());
-        
-        vector<int> res(queries.size());
-        for (int i = 0; i < queries.size(); i++)
-            res[i] = ranges::upper_bound(GCD, queries[i]) - GCD.begin();
-            
-        return res;
+
+        vector<long long> pairCnt(maxVal+1, 0);
+        for(int i = maxVal ; i >= 1 ; i--){
+            long long cnt = divisorCnt[i];
+
+            // nc2
+            pairCnt[i] = cnt*(cnt-1)/2;
+
+            // we will have to remove the count , if its higher multiple has any count
+            for(int k = 2*i ; k <= maxVal ; k += i){
+                pairCnt[i] -= pairCnt[k];
+            }
+        }
+
+        vector<long long> prefixCntGcd(maxVal+1, 0);
+        for(int i = 1 ; i <= maxVal ; i++){
+            prefixCntGcd[i] = prefixCntGcd[i-1]+pairCnt[i];
+        }
+
+        vector<int> result;
+
+        for(long long idx : queries){
+            int l = 0 , r = maxVal;
+            int ans = 1;
+
+            while(l <= r){
+                int mid = l+(r-l)/2;
+
+                if(prefixCntGcd[mid] > idx){
+                    ans = mid;
+                    r = mid-1;
+                }
+                else l = mid+1;
+            }
+
+            result.push_back(ans);
+        }
+
+        return result;
     }
 };
