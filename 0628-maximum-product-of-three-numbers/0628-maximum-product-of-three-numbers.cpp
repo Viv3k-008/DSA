26b6a422819bf6309a76bc9e366ee985d8fac231
Vivek Kumar Singh <singhvivg2n@gmail.com>
2026-07-26 14:16:52 +0530

    Time: 1 ms (71.43%), Space: 31.4 MB (76.45%) - LeetHub

--- 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.cpp
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.cpp
@@ -3,8 +3,38 @@ public:
     int maximumProduct(vector<int>& nums) {
         int n = nums.size();
 
-        sort(nums.begin(), nums.end());
+        int posFirst = INT_MIN, posSecond = INT_MIN, posThird = INT_MIN;
+        int negFirst = INT_MAX, negSecond = INT_MAX, negThird = INT_MAX;
 
-        return max(nums[0]*nums[1]*nums[n-1] , nums[n-1]*nums[n-2]*nums[n-3]);
+        for(int i : nums){
+            if(i > posFirst){
+                posThird = posSecond;
+                posSecond = posFirst;
+                posFirst = i;
+            }
+            else if(i > posSecond){
+                posThird = posSecond;
+                posSecond = i;
+            }
+            else if(i > posThird){
+                posThird = i;
+            }
+
+            if(negFirst > i){
+                negThird = negSecond;
+                negSecond = negFirst;
+                negFirst = i;
+            }
+            else if(negSecond > i){
+                negThird = negSecond;
+                negSecond = i;
+            }
+            else if(negThird > i){
+                negThird = i;
+            }
+        }
+
+
+        return max(posFirst*posSecond*posThird, negFirst*negSecond*posFirst);
     }
 };
