20dd49f163ecb95518f010fc0d7c51b8f4d433c2
Vivek Kumar Singh <singhvivg2n@gmail.com>
2026-07-31 14:37:21 +0530

    Time: 14 ms (37.38%), Space: 28.5 MB (25.39%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
-
     int minimumPushes(string word) {
         int n = word.size();
 
-        int count = 0, alphaCnt = 0, add = 1;
+        int alphaCnt = 0, ans = 0, add = 1;
 
-        vector<int> freq(1e5+1,0);
+        vector<int> alpha(26, 0);
 
-        vector<int> alpha(26,0);
-        for(char c : word) alpha[c-'a']++;
+        for(int i = 0 ; i < n ; i++){
+            alpha[word[i]-'a']++;
+        }
 
-        for(int c : alpha) freq[c]++;
+        sort(alpha.rbegin(),alpha.rend());
 
-        vector<int> freqWord;
-
-
-        for(int i = 1e5 ; i > 0 ; i--){
-            while(freq[i]--){
+        for(int i : alpha){
+            if(i > 0){
                 alphaCnt++;
-                count += i*add;
+
+                ans += i*add;
+
                 if(alphaCnt%8 == 0) add++;
             }
         }
-        return count;
+
+        return ans;
     }
 };
