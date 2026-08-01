05b83b2f26ba171b1c2d8a8007c7102183ae91d9
Vivek Kumar Singh <singhvivg2n@gmail.com>
2026-08-01 15:17:50 +0530

    Time: 0 ms (100%), Space: 10.1 MB (45.39%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -5,7 +5,8 @@ public:
         if(l > r) return 0;
 
         if(dp[l][r] != -1) return dp[l][r];
-        int op1 = nums[l] + min(fn(l+2, r, nums) , fn(l+1, r-1, nums));
+
+        int op1 = nums[l] + min(fn(l+2, r, nums) , fn(l+1, r-1, nums));    // Taking min here because the opponent will also get chance and he will also take the max one so the left will be the min one that you can take for the player A.
         int op2 = nums[r] + min(fn(l+1, r-1, nums) , fn(l, r-2, nums));
 
         return dp[l][r] = max(op1, op2);
