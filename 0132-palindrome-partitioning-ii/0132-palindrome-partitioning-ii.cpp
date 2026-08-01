be531b226ab69bd8061113eccbfc07eaa11c09ec
Vivek Kumar Singh <singhvivg2n@gmail.com>
2026-08-01 22:42:51 +0530

    Time: 1942 ms (5.02%), Space: 9.4 MB (66.61%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -1,38 +1,34 @@
 class Solution {
 public:
-    bool check(int i , int j , string& s){
-        while(i < j){
-            if(s[i++] != s[j--]){
+    bool check(int l, int r, string& s){
+        while(l <= r){
+            if(s[l++] != s[r--]){
                 return false;
             }
         }
-
         return true;
     }
-    int fn(int i , int n , string& s , vector<int>& dp){
+
+    vector<int> dp;
+    int fn(int i, int n, string& s){
         if(i == n) return 0;
-        
+
         if(dp[i] != -1) return dp[i];
 
-        int mini = INT_MAX;
-        for(int k = i ; k < n ; k++){
-            int cur = INT_MAX;
-            if(check(i , k , s)){
-                cur = 1 + fn(k+1 , n , s , dp);
+        int cuts = 1e9;
+        for(int k = i; k < n ; k++){
+            if(check(i, k, s)){
+                cuts = min(cuts, 1 + fn(k+1, n, s));
             }
-
-            mini = min(mini , cur);
         }
 
-        return dp[i] = mini;
+        return dp[i] = cuts;
     }
     int minCut(string s) {
         int n = s.size();
 
-        if(n == 1) return 0;
+        dp.resize(n, -1);
 
-        vector<int> dp(n , -1);
-
-        return fn(0 , n , s , dp)-1;
+        return fn(0, n, s)-1;
     }
 };
