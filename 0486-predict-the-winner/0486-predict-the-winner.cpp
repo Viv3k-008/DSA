efe3b51dc1a545a2782a79f3cbfcf262be060c18
Vivek Kumar Singh <singhvivg2n@gmail.com>
2026-08-01 14:27:17 +0530

    Time: 4 ms (30.84%), Space: 10.2 MB (45.39%) - LeetHub

---
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<vector<int>> dp;
+    int fn(int l, int r, vector<int>& nums){
+        if(l > r) return 0;
+
+        if(dp[l][r] != -1) return dp[l][r];
+        int op1 = nums[l] + min(fn(l+2, r, nums) , fn(l+1, r-1, nums));
+        int op2 = nums[r] + min(fn(l+1, r-1, nums) , fn(l, r-2, nums));
+
+        return dp[l][r] = max(op1, op2);
+    }
+    bool predictTheWinner(vector<int>& nums) {
+        int n = nums.size(), l = 0, r = n-1;
+
+        int total = 0;
+        for(int i : nums) total += i;
+
+        dp.resize(n, vector<int>(n, -1));
+        int a = fn(l, r, nums);
+        int b = total - a;
+
+        return a >= b;
+    }
+};
