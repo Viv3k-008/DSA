243dfb35405e7073fefa02a57418ae24f7124b4c
Vivek Kumar Singh <singhvivg2n@gmail.com>
2026-08-03 23:34:56 +0530

    Time: 0 ms (100%), Space: 10.3 MB (10.98%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -6,21 +6,20 @@ public:
 
         if(dp[l][r] != -1) return dp[l][r];
 
-        int op1 = nums[l] + min(fn(l+2, r, nums) , fn(l+1, r-1, nums));    // Taking min here because the opponent will also get chance and he will also take the max one so the left will be the min one that you can take for the player A.
-        int op2 = nums[r] + min(fn(l+1, r-1, nums) , fn(l, r-2, nums));
+        int op1 = nums[l] - fn(l+1, r, nums);
+        int op2 = nums[r] - fn(l, r-1, nums);
 
         return dp[l][r] = max(op1, op2);
     }
     bool predictTheWinner(vector<int>& nums) {
         int n = nums.size(), l = 0, r = n-1;
 
-        int total = 0;
-        for(int i : nums) total += i;
+       
 
         dp.resize(n, vector<int>(n, -1));
         int a = fn(l, r, nums);
-        int b = total - a;
+       
 
-        return a >= b;
+        return a >= 0;
     }
 };
