b717e15b0babc85807cd3edd4e35bec4946cd3e3
Vivek Kumar Singh <singhvivg2n@gmail.com>
2026-08-03 20:49:47 +0530

    Time: 19 ms (14.35%), Space: 19.7 MB (11.02%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -1,6 +1,20 @@
 class Solution {
 public:
+    vector<vector<int>> dp;
+    int fn(int l, int r, vector<int>& piles){
+        if(l > r){
+            return 0;
+        }
+
+        if(dp[l][r] != -1) return dp[l][r];
+        int left = piles[l] - fn(l+1, r, piles);
+        int right = piles[r] - fn(l, r-1, piles);
+
+        return dp[l][r] = max(left, right);
+    }
     bool stoneGame(vector<int>& piles) {
-        return true;
+        int n = piles.size();
+        dp.resize(n, vector<int>(n, -1));
+        return fn(0, n-1, piles);
     }
 };
