b4d2a52ffc0c82aa52784a493d3f749ac5b2c50a
Vivek Kumar Singh <singhvivg2n@gmail.com>
2026-08-05 00:31:16 +0530

    Time: 19 ms (46.2%), Space: 12.2 MB (55.57%) - LeetHub

---
+++ 0664-strange-printer/0664-strange-printer.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int fn(int l, int r, string& s, vector<vector<int>>& dp){
+        if(l > r) return 0;
+        if(l == r) return 1;
+
+        if(dp[l][r] != -1) return dp[l][r];
+
+        int op1 = 1 + fn(l+1, r, s, dp);
+        int op2 = INT_MAX;
+        for(int k = l+1; k <= r; k++){
+            if(s[l] == s[k]){
+                int temp = fn(l+1, k-1, s, dp) + fn(k , r, s, dp);
+
+                op2 = min(temp, op2);
+            }
+        }
+
+        return dp[l][r] = min(op1, op2);
+    }
+    int strangePrinter(string s) {
+        int n = s.size();
+
+        vector<vector<int>> dp(n, vector<int>(n, -1));
+        return fn(0, n-1, s, dp);
+    }
+};
