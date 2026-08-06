667252457f78540a6d1af51337ad9d2d364aaca9
Vivek Kumar Singh <singhvivg2n@gmail.com>
2026-08-07 00:34:49 +0530

    Time: 0 ms (100%), Space: 9.9 MB (86.34%) - LeetHub

--- 0376-wiggle-subsequence/0376-wiggle-subsequence.cpp
+++ 0376-wiggle-subsequence/0376-wiggle-subsequence.cpp
@@ -1,33 +1,47 @@
+// DP arrpoach
+// int wiggleMaxLength(vector<int>& nums) {
+//     int n = nums.size();
+
+//     vector<int> dp1(n, 1), dp2(n, 1);
+
+//     // dp1 -> stores the max length of sequence when number is taking next element as greater
+//     // dp2 -> stores the max length of sequence when number is taking next element as smaller
+
+//     int ans = INT_MIN;
+
+//     for(int i = n-1; i >= 0; i--){
+//         // greater
+//         for(int k = i+1; k < n ; k++){
+//             if(nums[i] < nums[k]){
+//                 dp1[i] = max(dp1[i], 1+dp2[k]);
+//             }
+//         }
+
+//         // lower
+//         for(int k = i+1; k < n ; k++){
+//             if(nums[i] > nums[k]){
+//                 dp2[i] = max(dp2[i], 1+dp1[k]);
+//             }
+//         }
+
+//         ans = max({ans, dp1[i], dp2[i]});
+//     }
+
+//     return ans;
+// }
+
 class Solution {
 public:
     int wiggleMaxLength(vector<int>& nums) {
         int n = nums.size();
 
-        vector<int> dp1(n, 1), dp2(n, 1);
+        int valley = 1, peak = 1;
 
-        // dp1 -> stores the max length of sequence when number is taking next element as greater
-        // dp2 -> stores the max length of sequence when number is taking next element as smaller
-
-        int ans = INT_MIN;
-
-        for(int i = n-1; i >= 0; i--){
-            // greater
-            for(int k = i+1; k < n ; k++){
-                if(nums[i] < nums[k]){
-                   dp1[i] = max(dp1[i], 1+dp2[k]);
-                }
-            }
-
-            // lower
-            for(int k = i+1; k < n ; k++){
-                if(nums[i] > nums[k]){
-                    dp2[i] = max(dp2[i], 1+dp1[k]);
-                }
-            }
-
-            ans = max({ans, dp1[i], dp2[i]});
+        for(int i = 1 ; i < n ; i++){
+            if(nums[i] > nums[i-1]) peak = valley+1;
+            else if(nums[i] < nums[i-1]) valley = peak+1;
         }
 
-        return ans;
+        return max(peak,valley);
     }
 };
