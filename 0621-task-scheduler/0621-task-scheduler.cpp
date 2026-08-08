838b439c9fc9b59ea034f99045393fd5d1dfbd5f
Vivek Kumar Singh <singhvivg2n@gmail.com>
2026-08-08 17:14:21 +0530

    Time: 3 ms (72.09%), Space: 38.3 MB (84.31%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.cpp
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -1,65 +1,24 @@
 class Solution {
 public:
-    void reduce(vector<int>& turn, int n, int val){
-        for(int i = 0 ; i < 26 ; i++){
-                if(turn[i] > 0){
-                    turn[i]--;
-                }
-            }
-            if(val != -1) turn[val] = n;
-    }
-    static bool comp(const pair<int,int> a,const pair<int,int> b) {
-        return a.second > b.second;
-    }
-
     int leastInterval(vector<char>& tasks, int n) {
-        // A -> B -> IDLE -> IDLE -> A -> B -> IDLE -> IDLE -> A -> B
+        int m = tasks.size();
 
-        multiset<pair<int,int>, decltype(&comp)> alpha(comp);
+        vector<int> alpha(26, 0);
 
-        unordered_map<int,int> cur;
-
-        for(char i : tasks) cur[i-'A']++;
-
-        int cnt = 0;
-        for(auto it : cur) alpha.insert(it), cnt += it.second;
-
-        vector<int> turn(26,0);
-
-        int ans = 0;
-        while(cnt){
-            int val = -1;
-            bool notFound = true;
-           for(auto it = alpha.begin(); it != alpha.end(); ++it) {
-
-                int character = it->first;
-                int frequency = it->second;
-
-                if(turn[character] == 0) {
-
-                    notFound = false;
-                    val = character;
-                    alpha.erase(it);
-                    frequency--;
-                    cnt--;
-                    if(frequency > 0) {
-                        alpha.insert({character, frequency});
-                    }
-
-                    break;
-                }
-            }
-            if(notFound){
-                ans++;
-                reduce(turn, n, -1);
-                continue;
-            }
-
-            ans++;
-            reduce(turn, n, val);
+        for(char i : tasks){
+            alpha[i-'A']++;
         }
 
-        return ans;
+        sort(alpha.begin(), alpha.end());
 
+        int maxFreq = alpha[25]-1; // gaps in which we will have to fill the chars
+        int gaps = maxFreq*n;      // and in that particular gap n spaces(chars) should be their
+
+        for(int i = 24; i >= 0 ; i--){
+            int mini = min(alpha[i], maxFreq);
+            gaps -= mini;
+        }
+
+        return (gaps > 0)? gaps+m : m;
     }
 };
