93e40c3e6e2ef2faf3e985a6ed1fafc3a77a05db
Vivek Kumar Singh <singhvivg2n@gmail.com>
2026-08-09 21:53:22 +0530

    Time: 16 ms (8.28%), Space: 27.6 MB (10.22%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,43 +1,30 @@
 class Solution {
 public:
     vector<vector<int>> dp;
-    // int fn(int i , int n , vector<int>& prices , int flag){
-    //     if(i == n){
-    //         return 0;
-    //     }
-
-    //     if(dp[i][flag] != -1) return dp[i][flag];
-
-    //     int op1 = 0 , op2 = 0;
-    //     if(flag){
-    //         op1 = -prices[i] + fn(i+1 , n , prices , 0);
-    //     }
-    //     else op1 = prices[i] + fn(i+1 , n , prices , 1);
-
-    //     op2 = fn(i+1 , n , prices , flag);
-
-    //     return dp[i][flag] = max(op1 , op2);
-    // }
-    int maxProfit(vector<int>& prices) {
-        int n = prices.size();
-        dp.resize(n+1 , vector<int>(2 , 0));
-        // return fn(0 , n , prices , 1);
-
-        for(int i = n-1 ; i >= 0 ; i--){
-            
-            for(int flag = 0 ; flag < 2 ; flag++){
-                int op1 = 0 , op2 = 0;
-                if(flag){
-                    op1 = max(-prices[i]+dp[i+1][0] , dp[i+1][1]);
-                }
-                else op1 = max(prices[i]+dp[i+1][1] , dp[i+1][0]);
-
-                op2 = dp[i+1][flag];
-
-                dp[i][flag] = max(op1 , op2);
-            }
+    int fn(int i, int n, vector<int>& p, bool buy){
+        if(i >= n){
+            if(buy) return 0;
+            else return -1e9;
         }
 
-        return dp[0][1];
+        if(dp[i][buy] != -1) return dp[i][buy];
+        int op = -1e9;
+        if(buy){
+            op = fn(i+1, n, p, false) - p[i];
+            op = max(op, fn(i+1, n, p, buy));
+        }
+        else{
+            op = fn(i+1, n, p, true) + p[i];
+            op = max(op, fn(i+1, n, p, buy));
+        }
+
+        return dp[i][buy] = op;
+
+    }
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+
+        dp.resize(n, vector<int>(2, -1));
+        return fn(0, n, prices, true);
     }
 };
