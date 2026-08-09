93c57e2a5d0f3f8b367358dbae585bd03323b54e
Vivek Kumar Singh <singhvivg2n@gmail.com>
2026-08-10 01:14:49 +0530

    Time: 17 ms (55.45%), Space: 12.2 MB (56.56%) - LeetHub

--- 0664-strange-printer/0664-strange-printer.cpp
+++ 0664-strange-printer/0664-strange-printer.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
     int fn(int l, int r, string& s, vector<vector<int>>& dp){
-        if(l > r) return 0;
-        if(l == r) return 1;
+       if(l > r) return 0;
+       if(l == r) return 1;
 
-        if(dp[l][r] != -1) return dp[l][r];
+       if(dp[l][r] != -1) return dp[l][r];
 
-        int op1 = 1 + fn(l+1, r, s, dp);
-        int op2 = INT_MAX;
-        for(int k = l+1; k <= r; k++){
-            if(s[l] == s[k]){
-                int temp = fn(l+1, k-1, s, dp) + fn(k , r, s, dp);
+       int op1 = 1 + fn(l+1, r, s, dp);
+       int op2 = INT_MAX;
+       for(int k = l+1 ; k <= r; k++){
+          if(s[l] == s[k]){
+             op2 = min(op2, fn(l+1, k-1, s, dp) + fn(k, r, s, dp));
+          }
+       }
 
-                op2 = min(temp, op2);
-            }
-        }
-
-        return dp[l][r] = min(op1, op2);
+       return dp[l][r] = min(op1, op2);
     }
     int strangePrinter(string s) {
         int n = s.size();
