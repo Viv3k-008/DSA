53a5c774e975fe616810a025ab7c80b124b1dae1
Vivek Kumar Singh <singhvivg2n@gmail.com>
2026-08-09 23:04:54 +0530

    Time: 35 ms (87.08%), Space: 18.7 MB (84.74%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.cpp
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -1,33 +1,38 @@
 class Solution {
 public:
-    bool check(string& numsi , string& numsj){
-        if(numsi.size() != numsj.size()+1) return false;
-        int i = 0 , j = 0;
-        while(i < numsi.size()){
-            if(numsi[i] == numsj[j]) i++ , j++;
-            else i++;
-        }
-        if(j == numsj.size()) return true;
-        else return false;
-        
-    }
-    bool static compareLength(string& a , string& b){
+    static bool comp(string& a, string& b){
         return a.size() < b.size();
+    }
+    int check(string &s1, string &s2){
+        int n = s1.size(), m = s2.size();
+
+        int i = 0, j = 0;
+        while(i < n && j < m){
+            if(s1[i] == s2[j]){
+                i++, j++;
+            }
+            else j++;
+        }
+
+        return i == n;
     }
     int longestStrChain(vector<string>& words) {
         int n = words.size();
-        int ans = INT_MIN;
-        sort(words.begin(),words.end() , compareLength);
-        vector<int> dp(n , 1);
+
+        sort(words.begin(), words.end(), comp);
+
+        vector<int> dp(n, 1);
+        int ans = 0;
         for(int i = 0 ; i < n ; i++){
-            for(int j = i-1 ; j >= 0 ; j--){
-                if(check(words[i] , words[j]) && dp[j]+1 > dp[i]){
-                    dp[i] = dp[j]+1;
+            for(int j = i-1; j >= 0 ; j--){
+                int l = words[i].size(), m = words[j].size();
+                if(l-m == 1){
+                    if(check(words[j], words[i]) &&( dp[j]+1 > dp[i])){
+                        dp[i] = dp[j]+1;
+                    }
                 }
             }
-            if(dp[i] > ans){
-               ans = dp[i];
-            }
+            ans = max(ans, dp[i]);
         }
 
         return ans;
