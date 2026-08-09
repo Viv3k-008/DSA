eead184012d74e509ef6c4979e260488454d7e66
Vivek Kumar Singh <singhvivg2n@gmail.com>
2026-08-10 01:01:56 +0530

    Time: 83 ms (31.32%), Space: 11 MB (83.45%) - LeetHub

--- 0375-guess-number-higher-or-lower-ii/0375-guess-number-higher-or-lower-ii.cpp
+++ 0375-guess-number-higher-or-lower-ii/0375-guess-number-higher-or-lower-ii.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
-    int fn(int l, int r, vector<vector<int>>& dp){
+    vector<vector<int>> dp;
+    int fn(int l, int r){
         if(l >= r) return 0;
 
         if(dp[l][r] != -1) return dp[l][r];
-
-        int mini = 1e9;
+        int cur = INT_MAX;
         for(int k = l; k <= r; k++){
-            mini = min(k + max(fn(l, k-1, dp), fn(k+1, r, dp)), mini);
+            cur = min(cur, k+max(fn(l, k-1), fn(k+1, r)));
         }
 
-        return dp[l][r] = mini;
+        return dp[l][r] = cur;
     }
     int getMoneyAmount(int n) {
-
-        vector<vector<int>> dp(n+1, vector<int>(n+1, -1));
-        return fn(1, n, dp);
+        dp.resize(n+1, vector<int>(n+1, -1));
+        return fn(1, n);
     }
 };
