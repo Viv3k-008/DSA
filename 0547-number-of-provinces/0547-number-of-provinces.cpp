4774cc12c67e1aece5a0680830826f173ab3f422
Vivek Kumar Singh <singhvivg2n@gmail.com>
2026-08-20 21:24:55 +0530

    Time: 0 ms (100%), Space: 21 MB (12.3%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,63 +1,34 @@
-class DisjointSetUnion{
-        public:
-        vector<int> par;
-        vector<int> size;
-        int components;
-        DisjointSetUnion(int n){
-            par.resize(n);
-            size.resize(n , 1);
-            components = n;
-            for(int i = 0 ; i < n ; i++){
-                par[i] = i;
-            }
-        }
-        
-        int root(int i){
-            if(i == par[i]){
-                return i;
-            }
-            
-            return par[i] = root(par[i]);
-        }
-        
-        int find(int u , int v){
-            return root(u) == root(v);
-        }
-        
-        bool unionNode(int i , int j){
-            int u = root(i);
-            int v = root(j);
-            
-            if(u == v){
-                return false;
-            }
-            
-            if(size[u] > size[v]){
-                swap(u , v);  // u tree size should be smaller
-            }
-            
-            par[u] = v;
-            size[v] += size[u];
-            components -= 1;
-            return true;
-        }
-        
-};
 class Solution {
 public:
+    void dfs(int src,  vector<vector<int>>& adj, vector<bool>& vis){
+        for(int num : adj[src]){
+            if(vis[num]) continue;
+            vis[num] = true;
+            dfs(num, adj, vis);
+        }
+    }
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n = isConnected.size();
-
-        DisjointSetUnion dsu(n);
-
+        
+        vector<vector<int>> adj(n);
         for(int i = 0 ; i < n ; i++){
             for(int j = 0 ; j < n ; j++){
-                if(isConnected[i][j]){
-                    dsu.unionNode(i,j);
+                if(isConnected[i][j] == 1){
+                    adj[i].push_back(j);
+                    adj[j].push_back(i);
                 }
             }
         }
 
-        return dsu.components;
+        vector<bool> vis(n, false);
+        int ans = 0;
+        for(int i = 0 ; i < n ; i++){
+            if(!vis[i]){
+                dfs(i, adj, vis);
+                ans++;
+            }
+        }
+
+        return ans;
     }
 };
