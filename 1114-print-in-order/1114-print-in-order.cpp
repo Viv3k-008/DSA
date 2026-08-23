ddc1b7fbb2e3c8adb6dbe6ce6a2f63011f764086
Vivek Kumar Singh <singhvivg2n@gmail.com>
2026-08-23 22:36:54 +0530

    Time: 11 ms (28.76%), Space: 9.5 MB (47.42%) - LeetHub

--- 1114-print-in-order/1114-print-in-order.cpp
+++ 1114-print-in-order/1114-print-in-order.cpp
@@ -1,37 +1,48 @@
 class Foo {
-    condition_variable cv;
-    mutex m;
-    int turn;
 public:
+    mutex m;
+    condition_variable cv;
+    int turn;
     Foo() {
         turn = 0;
     }
 
     void first(function<void()> printFirst) {
+        unique_lock<mutex> lock(m);
+
+        while(turn%3 != 0){
+            cv.wait(lock);
+        }
         // printFirst() outputs "first". Do not change or remove this line.
         printFirst();
-        turn = 1;
+        turn++;
+
         cv.notify_all();
     }
 
     void second(function<void()> printSecond) {
         unique_lock<mutex> lock(m);
-        while(turn != 1){
+
+        while(turn%3 != 1){
             cv.wait(lock);
         }
-        turn = 2;
         // printSecond() outputs "second". Do not change or remove this line.
         printSecond();
+        turn++;
 
         cv.notify_all();
     }
 
     void third(function<void()> printThird) {
         unique_lock<mutex> lock(m);
-        while(turn != 2){
+
+        while(turn%3 != 2){
             cv.wait(lock);
         }
         // printThird() outputs "third". Do not change or remove this line.
         printThird();
+        turn++;
+
+        cv.notify_all();
     }
 };
