80e8de4af3babbaf1d334158393d15ee3906885c
Vivek Kumar Singh <singhvivg2n@gmail.com>
2026-09-03 15:25:49 +0530

    Time: 72 ms (14.66%), Space: 165.9 MB (62.83%) - LeetHub

--- 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
@@ -6,10 +6,14 @@ public:
         sort(nums1.begin(), nums1.end());
 
         if(nums1[0]%2) return true;
+        // both even or odd can be converted to different parity by substracting with odd.
+        // if smallest number is odd then we can convert every number to odd.
 
         for(int i : nums1){
             if(i%2) return false;
         }
+        // if the smallest is even, so every even can't be converted to odd and if their are some odd so that also will not be converted to odd.
+        // answer can be true if and only if their are only even  if the smallest element is even.
 
         return true;
     }
