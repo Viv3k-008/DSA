a4d224c4937b8a5c26fa5bd05d212b34e24ca11c
Vivek Kumar Singh <singhvivg2n@gmail.com>
2026-09-08 13:58:23 +0530

    Time: 0 ms (100%), Space: 8.5 MB (84.49%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.cpp
+++ 3870-count-commas-in-range/3870-count-commas-in-range.cpp
@@ -1,10 +1,6 @@
 class Solution {
 public:
     int countCommas(int n) {
-        int cnt = 0;
-
-        if(n < 1000) return 0;
-
-        return n-999;
+        return (n < 1000)? 0 : n-999;
     }
 };
