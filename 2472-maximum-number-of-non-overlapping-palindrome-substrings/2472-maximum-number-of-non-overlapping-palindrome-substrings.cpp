21a5dfd35ef304a0797b028a47077cb2eced6837
Vivek Kumar Singh <singhvivg2n@gmail.com>
2026-09-15 15:00:29 +0530

    Time: 151 ms (52.36%), Space: 19.6 MB (34.82%) - LeetHub

--- 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.cpp
+++ 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.cpp
@@ -1,33 +1,44 @@
 class Solution {
 public:
-    bool check(int i, int j, string& s){
-        while(i < j){
-            if(s[i] != s[j]){
-                return false;
+    void expand(int i, int j, string& s, vector<vector<bool>>& check){
+        while(i >= 0 && j < s.size()){
+            if(s[i] == s[j]){
+                check[i][j] = true;
+                i--,j++;
             }
-            i++, j--;
+            else break;
         }
-        return true;
     }
     vector<int> dp;
-    int fn(int i, int k, int n, string& s){
+    int fn(int i, int k, int n, string& s, vector<vector<bool>>& check){
         if(i == n) return 0;
 
         if(dp[i] != -1) return dp[i];
 
         int op1 = 0;
         for(int j = i+k-1; j < n ; j++){
-            if(j-i+1 >= k && check(i, j, s)){
-                op1 = max(op1, 1 + fn(j+1, k, n, s));
+            if(j-i+1 >= k && check[i][j]){
+                op1 = max(op1, 1 + fn(j+1, k, n, s, check));
             }
         }
 
-        int op2 = fn(i+1, k, n, s);
+        int op2 = fn(i+1, k, n, s, check);
         return dp[i] = max(op1, op2);
     }
     int maxPalindromes(string s, int k) {
         int n = s.size();
         dp.resize(n, -1);
-        return fn(0, k, n, s);
+
+        vector<vector<bool>> check(n, vector<bool>(n, false));
+        for(int i = 0; i < n ; i++){
+            if(i == n-1){
+                expand(i, i, s, check);
+            }
+            else {
+                expand(i, i, s, check);
+                expand(i, i+1, s, check);
+            }
+        }
+        return fn(0, k, n, s, check);
     }
 };
