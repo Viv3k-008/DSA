d5fe234946df6038408d39f1fcf2ae938af58731
Vivek Kumar Singh <singhvivg2n@gmail.com>
2026-09-16 21:43:25 +0530

    Time: 40 ms (55.69%), Space: 19.4 MB (32.67%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -5,14 +5,13 @@ public:
 
         unordered_map<char,int> mp;
 
-        int l = 0 , ans = 0;
+        int l = -1, ans = 0;
         for(int i = 0 ; i < n ; i++){
             if(mp.count(s[i])){
-                if(mp[s[i]] >= l) l = mp[s[i]]+1;
+                l = max(l , mp[s[i]]);
             }
-
-            ans = max(ans , i-l+1);
             mp[s[i]] = i;
+            ans = max(ans, i-l);
         }
 
         return ans;
