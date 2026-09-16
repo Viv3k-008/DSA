af437109fd6c8e22958de42ec945c3a6fa649d47
Vivek Kumar Singh <singhvivg2n@gmail.com>
2026-09-16 22:17:18 +0530

    Time: 214 ms (5.02%), Space: 121.6 MB (5.2%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -3,21 +3,34 @@ public:
     int longestConsecutive(vector<int>& nums) {
         int n = nums.size();
 
-        unordered_set<int> st(nums.begin(),nums.end());
+        unordered_map<int,int> left, right, vis;
+
+        for(int i = n-1; i >= 0 ; i--){
+            right[nums[i]]++;
+        }
 
         int ans = 0;
-        for(int i : st){
-
-            int num = i, len = 1;
-            if(!st.count(num-1)){
-
-                while(num != 1e9 && st.count(num+1)){
-                    num++;
-                    len++;
-                }
+        for(int i = 0; i < n; i++){
+            right[nums[i]]--;
+            if(right[nums[i]] == 0){
+                right.erase(nums[i]);
             }
 
-            ans = max(ans , len);
+            if(left.count(nums[i]+1)){
+                ans = max(ans, left[nums[i]+1]+1);
+                left[nums[i]] = left[nums[i]+1]+1;
+            }
+
+            int cnt = 1, temp = nums[i]+1;
+            if(vis.count(temp)) continue;
+            while(right.count(temp)){
+                vis[temp]++;
+                cnt++;
+                temp++;
+            }
+            if(left.count(temp)) cnt += left[temp];
+            if(cnt != 0) left[nums[i]] = cnt;
+            ans = max(ans, cnt);
         }
 
         return ans;
