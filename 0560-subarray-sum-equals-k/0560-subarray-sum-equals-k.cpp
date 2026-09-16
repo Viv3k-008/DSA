158c4b2aa6238d6a189bfa3f14ac982f03e9cec3
Vivek Kumar Singh <singhvivg2n@gmail.com>
2026-09-16 22:46:07 +0530

    Time: 46 ms (48.87%), Space: 45.5 MB (55.12%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -4,17 +4,17 @@ public:
         int n = nums.size();
 
         unordered_map<int,int> mp;
-        mp[0]++;
-
-        int sum = 0, ans = 0;
-        for(int i = 0; i < n ; i++){
+        mp[0] = 1;
+        int sum = 0, cnt = 0;
+        for(int i = 0 ; i < n ; i++){
             sum += nums[i];
             if(mp.count(sum-k)){
-                ans += mp[sum-k];
+                cnt += mp[sum-k];
             }
+
             mp[sum]++;
         }
 
-        return ans;
+        return cnt;
     }
 };
